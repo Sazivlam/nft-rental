7b486668603f90fb896b5be1f1f84948a21253e3
Vlad Zhalin <vlad.zhalin97@gmail.com>
2021-11-04 19:25:41 +0100

    ERC721Enumerable

--- truffle/contracts/NFTRental.sol
+++ truffle/contracts/NFTRental.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTRental is Ownable, ERC721 {
+contract NFTRental is Ownable, ERC721Enumerable {
     using Counters for Counters.Counter;
     
     Counters.Counter private _tokenIds;
